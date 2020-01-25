6ea0f76e24a658378f077c32b58b1da8ef49f946
JGSims <josi3006@hotmail.com>
2020-01-25 01:27:25 -0500

    Can create manager and employee objects from CLI

---
+++ app.js
@@ -0,0 +1,122 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+// const Engineer = require('./lib/engineer');
+// const Intern = require('./lib/intern');
+
+
+
+async function makeTeam() {
+    await inquirer
+        .prompt(
+            {
+                message: 'What is your team name?',
+                name: 'teamname'
+            })
+
+        .then(function ({ teamname }) {
+
+            console.log(teamname + ' is a great name!');
+
+
+        })
+
+
+
+
+
+    await inquirer
+        .prompt([
+            {
+                message: 'What is the Team Manager\'s name?',
+                name: 'managername'
+            },
+            {
+                message: 'What is the Manager\'s ID#?',
+                name: 'managerID'
+            },
+            {
+                message: 'What is the Manager\'s Email?',
+                name: 'managerEmail'
+            },
+            {
+                message: 'What is the Manager\'s Office #?',
+                name: 'officeNumber'
+            }
+        ]).then(
+            function (manData) {
+
+                const name = manData.managername;
+                const id = manData.managerID;
+                const email = manData.managerEmail;
+                const officeNumber = manData.officeNumber;
+
+                const manager = new Manager(id, name, email, officeNumber);
+
+                console.log(manager);
+            })
+
+
+
+
+    await inquirer.prompt([
+        {
+            message: 'Enter an employee name: ',
+            name: 'empName'
+        },
+        {
+            message: 'Enter the employee ID#: ',
+            name: 'empID'
+        },
+        {
+            message: 'Enter the employee Email: ',
+            name: 'empEmail'
+        },
+        {
+            type: 'list',
+            message: 'Enter the employee Role: ',
+            name: 'empRole',
+            choices: ['Engineer', 'Intern']
+        }
+    ]).then(
+        function (empData) {
+            const name = empData.empName;
+            const id = empData.empID;
+            const email = empData.empEmail;
+            const role = empData.empRole;
+
+            const employee = new Employee(id, name, email, role);
+
+            console.log(employee);
+
+        }
+    )
+
+}
+
+
+
+
+
+makeTeam();
+
+
+// Prompt for Name
+
+// Prompt for ID
+
+// Promp for Email
+
+// Prompt for Role
+
+// If Role === Manager, check hasManager, then set hasManager to true or return 'already have', Prompt for Office Number
+
+// If Role === Engineer, Prompt for GitHub
+
+// If Role === Intern, Prompt for School
+
+// Prompt for more employees?   If yes, new instance, if no, render html
